6d53973f0cdef944b5c1d7ffaf80ba231d6eac2a
Alexey <volodalexey@gmail.com>
2017-05-04 13:56:56 +0300

    Implemented transform feedback for y coordinate

--- new/app.webgl2.js
+++ new/app.webgl2.js
@@ -31,73 +31,77 @@ Promise.all([
        * each constraint has 2 points
        * each constraints has 3 coordinates
        */
-      springsData = new Float32Array(cloth.points.length * 2 * 2 * 3),
-      vertexBuffer, backVertexBuffer;
+      springsData = new Float32Array(cloth.points.length * 2 * 2 * 4),
+      coordinatesVertexAttribute,
+      sourceVertexBuffer, targetVertexBuffer, tempVertexBuffer;
 
       let frames = 0;
     (function update() {
+      if (!sourceVertexBuffer) {
+        cloth.update({ delta: 0.016, canvas, pointer});
 
-      cloth.update({ delta: 0.016, canvas, pointer});
-
-      let iterator = 0, halfWidth = canvas.width / 2, halfHeight = canvas.height / 2;
-      for (let point of cloth.points) {
-        for (let constraint of point.constraints) {
-          springsData[iterator] = WebGL.translateCoord(constraint.p1.x, halfWidth);
-          springsData[iterator + 1] = -1 * WebGL.translateCoord(constraint.p1.y, halfHeight);
-          springsData[iterator + 2] = point.influenced ? 1 : 0;
-          springsData[iterator + 3] = WebGL.translateCoord(constraint.p2.x, halfWidth);
-          springsData[iterator + 4] = -1 * WebGL.translateCoord(constraint.p2.y, halfHeight);
-          springsData[iterator + 5] = point.influenced ? 1 : 0;
-          iterator += 6;
+        let iterator = 0, halfWidth = canvas.width / 2, halfHeight = canvas.height / 2;
+        for (let point of cloth.points) {
+          for (let constraint of point.constraints) {
+            springsData[iterator] = WebGL.translateCoord(constraint.p1.x, halfWidth);
+            springsData[iterator + 1] = -1 * WebGL.translateCoord(constraint.p1.y, halfHeight);
+            springsData[iterator + 2] = point.influenced ? 1 : 0;
+            springsData[iterator + 3] = 1.0; // vec4(.., .., .., 1.0)
+            springsData[iterator + 4] = WebGL.translateCoord(constraint.p2.x, halfWidth);
+            springsData[iterator + 5] = -1 * WebGL.translateCoord(constraint.p2.y, halfHeight);
+            springsData[iterator + 6] = point.influenced ? 1 : 0;
+            springsData[iterator + 7] = 1.0; // vec4(.., .., .., 1.0)
+            iterator += 8;
+          }
         }
-      }
-      let isTime = () => frames > 100;
 
-      if (!vertexBuffer) {
-        vertexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        sourceVertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, sourceVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, springsData, gl.DYNAMIC_COPY);
 
-        let coordinates = gl.getAttribLocation(shaderProgram, 'coordinates');
-        gl.vertexAttribPointer(coordinates, 3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(coordinates);
+        coordinatesVertexAttribute = gl.getAttribLocation(shaderProgram, 'coordinates');
+        gl.vertexAttribPointer(coordinatesVertexAttribute, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(coordinatesVertexAttribute);
       } else {
-        if (isTime()) {
-
-        } else {
-          gl.bufferData(gl.ARRAY_BUFFER, springsData, gl.DYNAMIC_COPY);
-        }
+        gl.bindBuffer(gl.ARRAY_BUFFER, sourceVertexBuffer);
+        gl.vertexAttribPointer(coordinatesVertexAttribute, 4, gl.FLOAT, false, 0, 0);
       }
       gl.clearColor(1, 1, 1, 1);
       gl.enable(gl.DEPTH_TEST);
       gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
       gl.viewport(0, 0, canvas.width, canvas.height);
 
-      if (isTime()) {
+      if (!targetVertexBuffer) {
         let transformFeedback = gl.createTransformFeedback();
         gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, transformFeedback);
 
-        backVertexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.TRANSFORM_FEEDBACK_BUFFER, backVertexBuffer);
+        targetVertexBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.TRANSFORM_FEEDBACK_BUFFER, targetVertexBuffer);
         gl.bufferData(gl.TRANSFORM_FEEDBACK_BUFFER, springsData.length * 6, gl.DYNAMIC_COPY);
-        gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 0, backVertexBuffer);
-        gl.beginTransformFeedback(gl.LINES);
-      }
-      gl.drawArrays(gl.LINES, 0, (iterator + 1) / 3);
-      if (isTime()) {
-        gl.endTransformFeedback();
-
-        let outPosition = new Float32Array(springsData.length);
-        gl.getBufferSubData(gl.TRANSFORM_FEEDBACK_BUFFER, 0, outPosition);
-        console.log(outPosition);
-      }
-      frames++;
-      if (isTime()) {
-        if (frames === 101) {
-          requestAnimationFrame(update)
-        }
       } else {
-        requestAnimationFrame(update)
+        gl.bindBuffer(gl.TRANSFORM_FEEDBACK_BUFFER, targetVertexBuffer);
+      }
+
+      gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 0, targetVertexBuffer);
+      gl.beginTransformFeedback(gl.LINES);
+
+      gl.drawArrays(gl.LINES, 0, springsData.length / 4);
+
+      gl.endTransformFeedback();
+
+      gl.bindBuffer(gl.ARRAY_BUFFER, null);
+      gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 0, null);
+
+      // let outPosition = new Float32Array(springsData.length);
+      // gl.getBufferSubData(gl.TRANSFORM_FEEDBACK_BUFFER, 0, outPosition);
+      // console.log(outPosition);
+
+      frames++;
+      tempVertexBuffer = sourceVertexBuffer;
+      sourceVertexBuffer = targetVertexBuffer;
+      targetVertexBuffer = tempVertexBuffer;
+      if (frames < 10) {
+        requestAnimationFrame(update);
       }
     })();
   });
